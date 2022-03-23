cb0b75d63f0c610a724ee88ba96c0a74cc412aca
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2022-03-23 16:39:25 +0100

    new idea

--- Assignment_1/camera_calibration.py
+++ Assignment_1/camera_calibration.py
@@ -1,31 +1,33 @@
+import numpy as np
+import cv2 
 
 # testprint
 print("hello world")
 
-import numpy as np
-import cv2 as cv
-import glob
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-images = glob.glob('*.jpg')
-for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture(0)
+
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+print("height : ", height)
+print("width: ", width)
+
+while(True):
+    img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
-cv.destroyAllWindows()
+    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+    # ret is a boolean that returns true if the frame is available.
+    # frame is an image array vector captured based on the default frames per second defined explicitly or implicitly
+    if(ret):    
+        print("yes")
+    if cv2.waitKey(10) == ord('q'):
+        break
+  
+# After the loop release the cap object
+cap.release()
+# # Destroy all the windows
+cv2.destroyAllWindows()
+
+# TODO release the video capture object and window
+
