a8038120365a29bc303546e5f0b85b86c2f2ce99
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2022-03-23 17:05:57 +0100

    saved one picture

--- Assignment_1/camera_calibration.py
+++ Assignment_1/camera_calibration.py
@@ -9,14 +9,18 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 print("height : ", height)
 print("width: ", width)
 foundCorners = 0
-while(foundCorners == 0):
+while(True):
     ret, img = cap.read()
     if(ret):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         cv2.imshow("webcam", gray)
         foundCorners, corners = cv2.findChessboardCorners(gray, (7,6), None)
-
-
+        if(foundCorners):
+            print("hello")
+            cv2.imwrite("newpage.png", gray)
+            break
+    if cv2.waitKey(10) == ord('q'):
+        break
 cap.release()
 cv2.destroyAllWindows()
 
