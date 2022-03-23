4bd664807cbb2d15e640bb12c60d87f63380fe2f
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2022-03-23 17:18:55 +0100

    working on save picture

--- Assignment_1/camera_calibration.py
+++ Assignment_1/camera_calibration.py
@@ -8,17 +8,23 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
 print("height : ", height)
 print("width: ", width)
-foundCorners = 0
+
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+objpoints = [] # 3d point in real world space
+
 #true hahahaha
 while(True):
+    i = 0
     ret, img = cap.read()
     if(ret):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         cv2.imshow("webcam", gray)
         foundCorners, corners = cv2.findChessboardCorners(gray, (7,6), None)
         if(foundCorners):
-            print("hello")
-            cv2.imwrite("newpage.png", gray)
+            cv2.imwrite("images/picture_"+ str(i) +".png", gray)
+            i+=1
             break
     if cv2.waitKey(10) == ord('q'):
         break
