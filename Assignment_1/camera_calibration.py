b225a9646f8998409ca658ebf54294e0b4e453b7
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2022-03-30 10:40:29 +0200

    exchanged numbers for width and height with variables

--- Assignment_1/camera_calibration.py
+++ Assignment_1/camera_calibration.py
@@ -23,10 +23,12 @@ cap.release()
 cv2.destroyAllWindows()
 
 # termination criteria
+chessboardWidth = 10
+chessboardHeight = 7
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((chessboardHeight*chessboardWidth,3), np.float32)
+objp[:,:2] = np.mgrid[0:chessboardWidth,0:chessboardHeight].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -38,14 +40,14 @@ for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (chessboardWidth,chessboardHeight), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
         corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv2.drawChessboardCorners(img, (chessboardWidth,chessboardHeight), corners2, ret)
         cv2.imshow('img', img)
         cv2.waitKey(5000)
 cv2.destroyAllWindows()
