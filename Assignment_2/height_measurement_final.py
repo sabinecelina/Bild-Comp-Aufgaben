b649698932ed1c4ed044b00568e7081e05c36a14
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2022-04-24 20:43:45 +0200

    edited final height measurement

--- Assignment_2/height_measurement_final.py
+++ Assignment_2/height_measurement_final.py
@@ -2,15 +2,17 @@ import numpy as np
 import cv2
 import operator
 
-
-def drawLine(image, lineColor=(255, 255, 255)):
+#lets a user draw a line on a given image
+#input: image
+#returns: new image, linepoint coordinates as shape (2,2) np.array
+def drawLine(image, lineColor = (255, 255, 255), thickness = 1):
     def mouseEventHandler(event, x, y, flags, parameters):
         def on_mouse_click(x, y):
             nonlocal editImage, line_coordinates, drawing
             if drawing:
                 line_coordinates.append((x, y))
                 cv2.line(
-                    editImage, line_coordinates[0], line_coordinates[1], lineColor, 2
+                    editImage, line_coordinates[0], line_coordinates[1], lineColor, thickness
                 )
             else:
                 line_coordinates.append((x, y))
@@ -20,7 +22,7 @@ def drawLine(image, lineColor=(255, 255, 255)):
             nonlocal editImage, line_coordinates, drawing
             if drawing:
                 editImage = image.copy()
-                cv2.line(editImage, line_coordinates[0], (x, y), lineColor, 2)
+                cv2.line(editImage, line_coordinates[0], (x, y), lineColor, thickness)
 
         match event:
             case cv2.EVENT_LBUTTONDOWN:
@@ -47,9 +49,43 @@ def drawLine(image, lineColor=(255, 255, 255)):
     cv2.destroyAllWindows()
     return line, editImage
 
+#inserts Point. If the point is not in the image it fills space with black. 
+#With the x and y offset the new image coordinates can be calculated.
+#input: image, point in image coordinates as (2) np.array
+#returns: new image, point in new image coordinates, x and y offset from original image
+def insertPointInImage(image, point, radius = 10, color = (255, 255, 255), border = 0):
+    height, width, _ = image.shape
+    x, y = point[0], point[1]
+    border = radius + border
+    # finding min and max x and y values needed for new image
+    min_x = min(x - border, 0)
+    max_x = max(x + border, width)
+    min_y = min(y - border, 0)
+    max_y = max(y + border, height)
+    # calculate size of new image
+    new_image_width = max_x - min_x
+    new_image_height = max_y - min_y
+    # fill new image with old image and draw point
+    new_image = np.zeros((new_image_height, new_image_width, 3), np.uint8)
+    offset = np.array([abs(min_x), abs(min_y)])
+    new_image[offset[1]:offset[1] + height, offset[0]:offset[0] + width, :] = image
+    new_point = point + offset
+    cv2.circle(new_image, new_point, radius, color, -1)
+    
+    return new_image, new_point, offset
+    
+
 
 image = cv2.imread("images/table_bottle_01.jpg")
-line, image = drawLine(image)
-print(line)
+cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+#line, image = drawLine(image)
+#cv2.imshow("image", image)
+#cv2.waitKey(0)
+#print(line)
+
+image, point, offset = insertPointInImage(image, np.array([-50,-50]),radius = 20, color = (255, 50, 50), border = 10)
+cv2.imshow("image", image)
+print(point, offset)
+cv2.waitKey(0)
 
 
