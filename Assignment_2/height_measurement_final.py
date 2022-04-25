dfdf7ab30d7e4dc882ee1022a9ff9d826cb26216
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2022-04-25 19:59:18 +0200

    edit variables and functions in python styleguide

--- Assignment_2/height_measurement_final.py
+++ Assignment_2/height_measurement_final.py
@@ -1,5 +1,3 @@
-from ssl import VERIFY_X509_PARTIAL_CHAIN
-from turtle import distance
 import numpy as np
 import cv2
 
@@ -9,14 +7,14 @@ import cv2
 # returns: new image, linepoint coordinates as shape (2,2) np.array
 
 
-def input_line(image, lineColor=(255, 255, 255), thickness=1):
+def input_line(image, line_color=(255, 255, 255), thickness=1):
     def mouseEventHandler(event, x, y, flags, parameters):
         def on_mouse_click(x, y):
             nonlocal edit_image, line_coordinates, drawing
             if drawing:
                 line_coordinates.append((x, y))
                 cv2.line(
-                    edit_image, line_coordinates[0], line_coordinates[1], lineColor, thickness)
+                    edit_image, line_coordinates[0], line_coordinates[1], line_color, thickness)
             else:
                 line_coordinates.append((x, y))
                 drawing = True
@@ -26,7 +24,7 @@ def input_line(image, lineColor=(255, 255, 255), thickness=1):
             if drawing:
                 edit_image = image.copy()
                 cv2.line(
-                    edit_image, line_coordinates[0], (x, y), lineColor, thickness)
+                    edit_image, line_coordinates[0], (x, y), line_color, thickness)
 
         match event:
             case cv2.EVENT_LBUTTONDOWN:
@@ -72,29 +70,31 @@ def insertPointInImage(image, point, radius=10, color=(255, 255, 255), border=0)
     new_image = np.zeros((new_image_height, new_image_width, 3), np.uint8)
     offset = np.array([abs(min_x), abs(min_y), 0])
     new_image[offset[1]:offset[1] + height,
-              offset[0]:offset[0] + width, :] = image
+    offset[0]:offset[0] + width, :] = image
     new_point = point + np.array([offset[0], offset[1], 1])
     cv2.circle(new_image, (round(new_point[0]), round(
         new_point[1])), radius, color, -1)
     # return new image, point in new coordinates and the offset from the original image
     return new_image, new_point, offset
 
+
 def get_intersection_point(a1, a2, b1, b2):
     point = np.cross(np.cross(a1, a2), np.cross(b1, b2))
     point = point / point[2]
     return np.array([point[0], point[1], 1])
 
+
 def get_new_image(image, v_x, v_y, b, r, b_0, t_0, v, t, v_z):
-    image, _, o = insertPointInImage(image, v_x, radius = 15, color=(255, 100, 0), border=10)
+    image, _, o = insertPointInImage(image, v_x, radius=15, color=(255, 100, 0), border=10)
     v_x, v_y, b, r, b_0, t_0, v, t, v_z = v_x + o, v_y + o, b + o, r + o, b_0 + 0, t_0 + o, v + o, t + o, v_z + o
 
-    image, _, o = insertPointInImage(image, v_y, radius = 15, color=(255, 100, 0), border=10)
+    image, _, o = insertPointInImage(image, v_y, radius=15, color=(255, 100, 0), border=10)
     v_x, v_y, b, r, b_0, t_0, v, t, v_z = v_x + o, v_y + o, b + o, r + o, b_0 + 0, t_0 + o, v + o, t + o, v_z + o
     return image
 
 
-def calculateCrossRatio(parallelLinePair_A, parallelLinePair_B, referenceObject, object, image):
-    editImage = image.copy()
+def calculate_cross_ratio(parallelLinePair_A, parallelLinePair_B, referenceObject, object, image):
+    edit_image = image.copy()
     v_x = get_intersection_point(
         parallelLinePair_A[0][0], parallelLinePair_A[0][1], parallelLinePair_A[1][0], parallelLinePair_A[1][1])
     v_y = get_intersection_point(
@@ -107,14 +107,8 @@ def calculateCrossRatio(parallelLinePair_A, parallelLinePair_B, referenceObject,
     t = get_intersection_point(v, t_0, r, b)
 
     v_z = get_intersection_point(t_0, b_0, r, b)
-    crossratio = (
-        np.linalg.norm(t - b) *
-        np.linalg.norm(v_z - r)
-    ) / (
-        np.linalg.norm(r - b) *
-        np.linalg.norm(v_z - t)
-    )
-    return crossratio, get_new_image(editImage, v_x, v_y, b, r, b_0, t_0, v, t, v_z)
+    cross_ratio = (np.linalg.norm(t - b) * np.linalg.norm(v_z - r)) / (np.linalg.norm(r - b) * np.linalg.norm(v_z - t))
+    return cross_ratio, get_new_image(edit_image, v_x, v_y, b, r, b_0, t_0, v, t, v_z)
 
 
 if __name__ == '__main__':
@@ -134,22 +128,22 @@ if __name__ == '__main__':
     Click to set the start or end point of a line.\n""")
     image = cv2.imread(path)
     original_image = image.copy()
-    lineA1, image = input_line(image, lineColor=(0, 255, 0), thickness=3)
-    lineA2, image = input_line(image, lineColor=(0, 255, 0), thickness=3)
+    lineA1, image = input_line(image, line_color=(0, 255, 0), thickness=3)
+    lineA2, image = input_line(image, line_color=(0, 255, 0), thickness=3)
     print("""\nIn the second step, mark two different parallel lines on the plane. 
     Those lines should not be parallel to the last two lines you marked.\n""")
-    lineB1, image = input_line(image, lineColor=(255, 0, 0), thickness=3)
-    lineB2, image = input_line(image, lineColor=(255, 0, 0), thickness=3)
+    lineB1, image = input_line(image, line_color=(255, 0, 0), thickness=3)
+    lineB2, image = input_line(image, line_color=(255, 0, 0), thickness=3)
     print("""\nNow mark the object from bottom to top.
     Note that the selection can be difficult depending on the shape of the object due to perspective effects.\n""")
-    lineC, image = input_line(image, lineColor=(0, 0, 255), thickness=3)
+    lineC, image = input_line(image, line_color=(0, 0, 255), thickness=3)
     print("""\nNow mark the reference object from bottom to top. Note that the selection can be difficult 
     depending on the shape of the object due to perspective effects.\n""")
-    lineD, image = input_line(image, lineColor=(0, 150, 200), thickness=3)
+    lineD, image = input_line(image, line_color=(0, 150, 200), thickness=3)
     cv2.destroyAllWindows()
     linePair_A = np.array([lineA1, lineA2])
     linePair_B = np.array([lineB1, lineB2])
-    cross_ratio, image = calculateCrossRatio(
+    cross_ratio, image = calculate_cross_ratio(
         linePair_A, linePair_B, lineD, lineC, image)
     print("\nEnter the height of the reference object in cm as a floating point number.\n")
     input_float = float(input())
