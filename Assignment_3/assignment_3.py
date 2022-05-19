1f067264f755b30bbf4fb8c9bd14328c0fbb4ab1
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2022-05-19 13:02:50 +0200

    visualising finding sift points

--- Assignment_3/assignment_3.py
+++ Assignment_3/assignment_3.py
@@ -1,4 +1,15 @@
 import cv2
+import numpy as np
+
+
+def visualize_sift_keypoints(_img, keypoint_one):
+    # Visualize the SIFT keypoints
+    keypointimage = cv2.drawKeypoints(_img, keypoint_one, None, color=(0, 255, 0),
+                                      flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    height, width, _ = keypointimage.shape
+    imS = cv2.resize(keypointimage, (int(height / 8), int(width / 8)))
+    return imS
+
 
 # load images
 imgL = cv2.imread("images/left.jpg", cv2.IMREAD_GRAYSCALE)
@@ -14,21 +25,42 @@ print('We found %d keypoints in the left image.' % len(keypoint_one))
 print('We found %d keypoints in the middle image.' % len(keypoint_two))
 print('We found %d keypoints in the right image.' % len(keypoint_three))
 
-# Visualize the SIFT keypoints
-keypointimage = cv2.drawKeypoints(imgL, keypoint_one, None, color=(0, 255, 0),
-                                  flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-height, width, _ = keypointimage.shape
-imS = cv2.resize(keypointimage, (int(height / 4), int(width / 4)))
-cv2.imshow('SIFT', imS)
+imgL = visualize_sift_keypoints(imgL, keypoint_one)
+imgM = visualize_sift_keypoints(imgM, keypoint_two)
+imgR = visualize_sift_keypoints(imgR, keypoint_three)
+
+numpy_vertical = np.hstack((imgR, imgM, imgL))
+cv2.imshow('Numpy Vertical', numpy_vertical)
 cv2.waitKey()
 
-# match the keypoints using a FlannBasedMatcher
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-search_params = dict(checks=50)  # or pass empty dictionary
 
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(descriptor_one, descriptor_two, descriptor_three, k=2)
-matchesMask = [[0, 0] for i in range(len(matches))]
-pts1 = []
-pts2 = []
+def find_matching_points(keypoint_one, keypoint_two):
+    # match the keypoints using a FlannBasedMatcher
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)  # or pass empty dictionary
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(descriptor_one, descriptor_two, descriptor_three, k=2)
+    matchesMask = [[0, 0] for i in range(len(matches))]
+    pts1 = []
+    pts2 = []
+
+    # ratio test as per Lowe's paper
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            matchesMask[i] = [1, 0]
+            pts2.append(keypoint_two[m.trainIdx].pt)
+            pts1.append(keypoint_one[m.queryIdx].pt)
+
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+    print('We found %d matching keypoints in both images.' % len(pts1))
+
+    # Compute the Fundamental Matrix.
+
+    F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+    # We select only inlier points
+    pts1 = pts1[mask.ravel() == 1]
+    pts2 = pts2[mask.ravel() == 1]
+    return pts1, pts2
